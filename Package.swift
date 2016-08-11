8663f1536b9cd399c3492cdad0d4619a2b08de5b
Ryan Collins <rymcol@users.noreply.github.com>
2016-08-11 11:26:28 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,13 @@
 import PackageDescription
 
 let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
-let swiftyJsonVersion = 6
-let swiftyJsonMinorVersion = 1
 
 let package = Package(
   name: "Mustache",
   dependencies: [
     .Package(url: "https://github.com/rymcol/Bridging.git", majorVersion: 0, minor: 10),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion, minor: swiftyJsonMinorVersion)
+    .Package(url: swiftyJsonUrl, majorVersion: 11, minor: 0)
   ],
   exclude: ["Tests/Carthage"]
 )
