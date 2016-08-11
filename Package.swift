26704ee130749ae943a79146d143a3018629aeb7
Ryan Collins <rymcol@users.noreply.github.com>
2016-08-11 11:00:29 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let swiftyJsonMinorVersion = 1
 let package = Package(
   name: "Mustache",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2),
+    .Package(url: "https://github.com/rymcol/Bridging.git", majorVersion: 0, minor: 10),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
     .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion, minor: swiftyJsonMinorVersion)
   ],
